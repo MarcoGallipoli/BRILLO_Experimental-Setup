c4dd4714ad97d97938efc729a7af15487304c63a
Marco Gallipoli <marcogallipoli01@gmail.com>
2023-11-10 19:03:59 +0100

    Aruco Detection Package Upload

---
+++ aruco_marker_detection/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/marco/aruco_marker_detection/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/marco/aruco_marker_detection/devel;/opt/ros/noetic'.split(';') if '/home/marco/aruco_marker_detection/devel;/opt/ros/noetic' != '' else []
---
+++ aruco_marker_detection/build/usb_cam/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/marco/aruco_marker_detection/src/usb_cam
+# Build directory: /home/marco/aruco_marker_detection/build/usb_cam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
